39ce6381580fae3b1489da8980928262689715fb
yumi-abe <yumibuhibuhi0610@gmail.com>
2024-03-17 22:20:29 +0900

    section6-90

--- app/Http/Controllers/Testcontroller.php
+++ app/Http/Controllers/Testcontroller.php
@@ -3,12 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Test;
+use Illuminate\Support\Facades\DB;
 
 class Testcontroller extends Controller
 {
     public function index()
     {
-        return view('tests.test');
+        //Eloquant(エロクアント)
+        $values = Test::all();
+
+        $count = Test::count();
+
+        $first = Test::findOrFail(1);
+
+        $whereBBB = Test::where('text', '=', 'bbb')->get();
+
+        //クエリビルダ
+        $queryBuilder = DB::table('tests')->where('text', '=', 'bbb')
+        ->select('id', 'text')
+        ->get();
+
+        dd($values, $count, $first, $whereBBB, $queryBuilder);
+
+        return view('tests.test', compact('values'));
     }
 }
